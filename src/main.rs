a6492435245ef18f1c83d429958556ecd4d52220
Brian Hicks <brian@brianthicks.com>
2020-11-17 08:00:31 -0600

    make a command-line interface that matches the README

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,45 @@ use tree_sitter;
 
 #[derive(Debug, Clap)]
 struct Options {
-    /// Imports to forbid
-    forbid: Vec<String>,
-
     /// The path to an Elm project (a folder containing elm.json) where
     /// we'll start looking for imports.
     #[clap(short, long, default_value = ".")]
     root: PathBuf,
+
+    /// The file where we'll store configuration about forbidden imports
+    /// and todos.
+    #[clap(short, long, default_value = "forbidden-imports.toml")]
+    config: PathBuf,
+
+    #[clap(subcommand)]
+    mode: Mode,
+}
+
+#[derive(Debug, Clap)]
+enum Mode {
+    /// Forbid a new import
+    Forbid {
+        /// The fully-qualified name to forbid (e.g. `Html.Events`)
+        name: String,
+
+        /// An additional string to print when showing an error for this
+        /// import. (idea: what should we do instead? Import something else? Use
+        /// another approach? Give up and buy a farm?)
+        #[clap(short, long)]
+        hint: Option<String>,
+    },
+
+    /// Check what imports still need to be cleaned up
+    Todo {
+        /// Which names, if any, to specifically check up on
+        names: Vec<String>,
+    },
+
+    /// Check that no uncontrolled imports are present (use me in CI!)
+    Check,
+
+    /// Update the allowed imports list
+    Update,
 }
 
 fn main() {

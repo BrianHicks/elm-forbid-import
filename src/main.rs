299f60cb2c1177f7c80cab392b23abe26189fcca
Brian Hicks <brian@brianthicks.com>
2020-11-17 05:38:07 -0600

    load tree sitter stuff in main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use tree_sitter;
+
 fn main() {
     println!("Hello, world!");
+
+    let _parser = get_parser();
+}
+
+// tree sitter
+
+extern "C" {
+    fn tree_sitter_elm() -> tree_sitter::Language;
+}
+
+fn get_parser() -> Result<tree_sitter::Parser, tree_sitter::LanguageError> {
+    let mut parser = tree_sitter::Parser::new();
+
+    let elm = unsafe { tree_sitter_elm() };
+    parser.set_language(elm)?;
+
+    Ok(parser)
 }

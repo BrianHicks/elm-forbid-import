d6c0682658d1806da24206ce5a57de08c0cf4bf1
Brian Hicks <brian@brianthicks.com>
2020-11-19 20:21:46 -0600

    diff between current and golden tests

--- script/run-integration-tests.sh
+++ script/run-integration-tests.sh
@@ -6,8 +6,13 @@ run_test() {
   TEST_FILE="${1:-}"
   NAME="$(basename "$TEST_FILE")"
 
+  GOLDEN="tests/golden-results/$NAME.txt"
+  CURRENT="$GOLDEN.current"
+
   echo "===== $NAME"
-  env PATH="$(pwd)/target/debug:$PATH" bash -xeou pipefail "$TEST_FILE" > "tests/golden-results/$NAME.txt"
+  env PATH="$(pwd)/target/debug:$PATH" bash -xeou pipefail "$TEST_FILE" > "$CURRENT"
+
+  diff -U 0 "$GOLDEN" "$CURRENT"
 }
 
 cargo build

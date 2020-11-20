66a59812876f248872df272c77286173c4667b09
Brian Hicks <brian@brianthicks.com>
2020-11-19 20:43:26 -0600

    fix error in promotion script

--- script/promote-golden-tests.sh
+++ script/promote-golden-tests.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -eou pipefail
 
-for $TO_PROMOTE in $(find tests/golden-results -type f -name '*.current'); do
+for TO_PROMOTE in $(find tests/golden-results -type f -name '*.current'); do
   mv "$TO_PROMOTE" "$(echo "$TO_PROMOTE" | sed -E 's/.current$//')"
 done

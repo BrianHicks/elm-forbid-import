2c3974f4569a8a0a015412d13217dc5aa2ca66c7
Brian Hicks <brian@brianthicks.com>
2020-11-20 08:52:38 -0600

    add a test for what it looks like when you remove an import

---
+++ tests/integration/removed-forbidden.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+elm-forbid-import add-root vendor/elm-spa-example
+elm-forbid-import forbid Html
+
+set +e
+MATCH="$(elm-forbid-import --format json check | jq '.[0]')"
+set -e
+
+elm-forbid-import update
+
+FILE="$(jq -r '.file' <<< "$MATCH")"
+cp "$FILE" "$FILE.bak"
+sed -i "$(jq '.position.row' <<< "$MATCH")d" "$FILE"
+
+if elm-forbid-import check; then
+  exit 1 # this check should exit 1
+fi
+
+mv "$FILE.bak" "$FILE"

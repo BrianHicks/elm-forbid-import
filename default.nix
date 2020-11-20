b71d528f10e81020229f14414f9e018cbbe7ebe0
Brian Hicks <brian@brianthicks.com>
2020-11-20 06:18:03 -0600

    do some weird stuff to run the integration test script in the build

--- default.nix
+++ default.nix
@@ -3,5 +3,28 @@
 let
   naersk = pkgs.callPackage sources.naersk { };
   gitignore = import sources.gitignore { };
-in naersk.buildPackage (gitignore.gitignoreSource ./.)
+
+  source = gitignore.gitignoreSource ./.;
+
+  # naerk's checkPhase does not have the tests directory available in checkPhase,
+  # so we work around it with a separate derivation. There's probably a nicer
+  # way around this but I don't know it right now.
+  package = naersk.buildPackage source;
+in pkgs.stdenv.mkDerivation {
+  name = package.name;
+  version = package.version;
+  src = source;
+
+  buildInputs = [ package pkgs.jq ];
+  buildPhase = "true";
+
+  doCheck = true;
+  checkPhase = ''
+    env PATH="${package}/bin:$PATH" script/run-integration-tests.sh
+  '';
+
+  installPhase = ''
+    ln -s ${package} $out
+  '';
+}
 

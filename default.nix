ee8b5d7413096eec76bd1cc1a07f61320a791180
Brian Hicks <brian@brianthicks.com>
2020-12-01 12:19:57 -0600

    don't run checks when building

--- default.nix
+++ default.nix
@@ -3,28 +3,5 @@
 let
   naersk = pkgs.callPackage sources.naersk { };
   gitignore = import sources.gitignore { };
-
-  source = gitignore.gitignoreSource ./.;
-
-  # naerk's checkPhase does not have the tests directory available in checkPhase,
-  # so we work around it with a separate derivation. There's probably a nicer
-  # way around this but I don't know it right now.
-  package = naersk.buildPackage source;
-in pkgs.stdenv.mkDerivation {
-  name = package.name;
-  version = package.version;
-  src = source;
-
-  buildInputs = [ package pkgs.jq ];
-  buildPhase = "true";
-
-  doCheck = true;
-  checkPhase = ''
-    env PATH="${package}/bin:$PATH" script/run-integration-tests.sh
-  '';
-
-  installPhase = ''
-    ln -s ${package} $out
-  '';
-}
+in naersk.buildPackage (gitignore.gitignoreSource ./.)
 

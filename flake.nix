61ea8d1dcced554c2fbe87805234cb6657146aa8
Brian Hicks <brian@brianthicks.com>
2023-06-12 16:30:02 -0500

    convert to a flake

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    naersk.url = "github:nix-community/naersk";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = inputs:
+    inputs.flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import inputs.nixpkgs { inherit system; };
+
+        naersk = pkgs.callPackage inputs.naersk {};
+      in {
+        defaultPackage = naersk.buildPackage {
+          src = ./.;
+        };
+
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            git
+
+            # Rust
+            cargo
+            rustPackages.clippy
+            rustPackages.rustfmt
+            rustc
+
+            # Benchmarking + Optimization
+            hyperfine
+
+            # Testing
+            jq
+          ];
+        };
+      }
+    );
+}
